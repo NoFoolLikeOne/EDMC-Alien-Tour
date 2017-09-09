1cb6022380ca7aa267b1b686b16cc5b8ba869012
Paulo Rodrigues <nofool@paparomeo.uk>
2017-09-09 23:34:41 +0100

    Change site structure
    Also added commander data and location event handlers

--- load.py
+++ load.py
@@ -21,7 +21,7 @@ this.prep = {}
 myPlugin = 'Alien Tour'
 sites_file = []
 
-with open(os.path.realpath(os.path.dirname(os.path.realpath(__file__)))+'/sites.json') as sites_file: 
+with open(os.path.realpath(os.path.dirname(os.path.realpath(__file__)))+'/alien_sites.json') as sites_file: 
 	sites = json.load(sites_file)	
 	
 
@@ -49,7 +49,7 @@ def plugin_prefs(parent,cmdr,is_beta):
 	return frame
 
 def plugin_app(parent):
-	label = tk.Label(parent, text= myPlugin + ":")
+	label = tk.Label(parent, text=  "Next Tour:")
 	this.status = tk.Label(parent, anchor=tk.W, text="Ready")
 	
 		
@@ -71,40 +71,58 @@ def getDistance(x1,y1,z1,x2,y2,z2):
 def findNearest(jumpsystem,list):
 	nearest	= { 'distance': 999999, 'name': "Tour Completed" } 
 	n=999999
-	for sysrec in list:
+	for key,value in list.iteritems():
 		#print str(n) +  ">"  + str(sysrec['distance'])
-		d = getDistance(jumpsystem["x"],jumpsystem["y"],jumpsystem["z"],sysrec["x"],sysrec["y"],sysrec["z"])
-		if float(n) > float(d) and sysrec["visited"]==0:
+		d = getDistance(jumpsystem["x"],jumpsystem["y"],jumpsystem["z"],value["x"],value["y"],value["z"])
+		if float(n) > float(d) and value["visited"]==0:
 			try:
 				n = d
-				nearest=sysrec
+				nearest=key
 			except:
 				print exception
-	return nearest["name"],n,nearest["lat"],nearest["lon"],nearest["active"]	
+	return key,n,list[key]["lat"],list[key]["lon"],list[key]["active"],list[key]["body"],list[key]["text"]		
 	
 # Detect journal events
 def journal_entry(cmdr, system, station, entry):
 
-    if entry['event'] == 'FSDJump':
+	if entry['event'] == 'FSDJump':
 		this.jumpsystem = { "x": entry["StarPos"][0], "y": entry["StarPos"][1], "z": entry["StarPos"][2], "name": entry["StarSystem"] }	
 		print this.jumpsystem
-		nearest,distance,lat,lon,active = findNearest(this.jumpsystem,sites)
+		nearest,distance,lat,lon,active,body,text = findNearest(this.jumpsystem,sites)
 		print nearest
 		print distance
 		this.status['text'] = nearest + " (" + str(distance) +"ly)"
+		print "Commander Data"
+		
+	if entry['event'] == 'Location':
+		print "Location"
+		print entry
+		this.lastsystem = { "x": entry["StarPos"][0], "y": entry["StarPos"][1], "z": entry["StarPos"][2], "name": entry["StarSystem"] }
+		nearest,distance,lat,lon,active,body,text = findNearest(this.lastsystem,sites)
+		print nearest
+		print distance
+		this.status['text'] = nearest + " (" + str(distance) +"ly)"
+		print "Commander Data"
 	
 	
-					
+def edsmGetSystem(system):
+	url = 'https://www.edsm.net/api-v1/system?systemName='+system+'&showCoordinates=1'		
+	print url
+	r = requests.get(url)
+	s =  r.json()
+	print s
+	return { "x": s["coords"]["x"], "y": s["coords"]["y"], "z": s["coords"]["z"], "name": s["name"] }		
 	
 
 # Update some data here too
 def cmdr_data(data):
 	print "Commander Data"
-	this.status['text'] = "Commander Data"
-
-# Defines location (system)
-def setLocation(location):
-	# just clearing the screenshot location
-	this.status['text'] = "setLocation"
-
+	#print data
+	this.lastsystem = edsmGetSystem(data['lastSystem']['name'])
+	nearest,distance,lat,lon,active,body,text = findNearest(this.lastsystem,sites)
+	print nearest
+	print distance
+	print "setting status"
+	this.status['text'] = nearest + " (" + str(distance) +"ly)"
+	print "Commander Data"
 
