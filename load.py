789b7c3942f306d02887973ccc8e5faefae05b53
Paulo Rodrigues <nofool@paparomeo.uk>
2017-09-16 04:25:24 +0100

    Some improvements

--- load.py
+++ load.py
@@ -51,23 +51,21 @@ def mark_visited(event):
 	sites[this.nearest]["visited"] = 1
 	print sites[this.nearest]
 	this.nearest,distance,lat,lon,active,body,text,priority,x,y,z = findNearest(this.lastsystem,sites)
-	print this.nearest
-	print distance
-	this.status['text'] = this.nearest + " (" + str(distance) +"ly)"
-	this.status['url'] = 'https://www.edsm.net/show-system?systemName=%s' % quote_plus(nearest)
-	this.description["text"] = text
+	setStatus(nearest,distance,body,text,lat,long)
+	
+	
+		
 
 	
 def drop_priority(event):
+	print "drop_priority"
+	print max_priority(sites)+1
 	print this.lastsystem
 	sites[this.nearest]["priority"] = max_priority(sites)+1
 	print sites[this.nearest]
 	this.nearest,distance,lat,lon,active,body,text,priority,x,y,z = findNearest(this.lastsystem,sites)
-	print this.nearest
-	print distance
-	this.status['text'] = this.nearest + " (" + str(distance) +"ly)"
-	this.status['url'] = 'https://www.edsm.net/show-system?systemName=%s' % quote_plus(nearest)	
-	this.description["text"] = text
+	setStatus(nearest,distance,body,text,lat,long)
+	
 	
 def copy_text_to_clipboard(event):
 	window.clipboard_clear()  # clear clipboard contents
@@ -107,13 +105,27 @@ def plugin_app(parent):
 	this.cross.bind("<Button-1>", drop_priority)  
 	this.spacer = tk.Frame(this.frame)
 	this.description = tk.Label(this.frame)
+	this.body_label = tk.Label(this.frame, text=  "Body:")
+	this.body = tk.Label(this.frame)
+
 	
 	this.label.grid(row = 0, column = 0, sticky=tk.W)
 	this.status.grid(row = 0, column = 1, sticky=tk.W)
 	this.clipboard.grid(row = 0, column = 2, sticky=tk.W)
 	this.tick.grid(row = 0, column = 3, sticky=tk.W)
 	this.cross.grid(row = 0, column = 4, sticky=tk.W)
-	this.description.grid(row = 1, column = 0, columnspan=4, sticky=tk.W)
+	this.body_label.grid(row = 1, column = 0, sticky=tk.W)
+	this.body.grid(row = 1, column = 1, columnspan=3, sticky=tk.W)
+	this.description.grid(row = 2, column = 0, columnspan=4, sticky=tk.W)
+	
+	this.label.grid_remove()
+	this.status.grid_remove()
+	this.clipboard.grid_remove()
+	this.tick.grid_remove()
+	this.cross.grid_remove()
+	this.description.grid_remove()
+	this.body.grid_remove()
+	this.body_label.grid_remove()
 	#label.grid(row = 1, column = 0, sticky=tk.W)
 	#this.status.grid(row = 1, column = 1, sticky=tk.W)
 	#this.icon.pack(side=RIGHT)
@@ -133,50 +145,74 @@ def getDistance(x1,y1,z1,x2,y2,z2):
 	return round(sqrt(pow(float(x2)-float(x1),2)+pow(float(y2)-float(y1),2)+pow(float(z2)-float(z1),2)),2)	
 	
 def findNearest(jumpsystem,list):
+	#print list
 	nearest	= { 'distance': 999999, 'name': "Tour Completed" } 
 	n=999999
-	p=0
+	p=999999
 	for key,value in list.iteritems():
 		#print str(n) +  ">"  + str(sysrec['distance'])
 		d = getDistance(jumpsystem["x"],jumpsystem["y"],jumpsystem["z"],value["x"],value["y"],value["z"])
-		if value["visited"] == 0:
-			if  float(d) < float(n) and value["priority"] <= p:
+		#print key+" "+str(d)+" "+str(value["priority"])
+		if int(value["visited"]) == 0:
+			lower_priority=int(value["priority"]) < int(p)
+			closer=float(d) < float(n) and int(value["priority"]) == int(p)
+			if  lower_priority or closer:			
 				try:
 					n = d
-					p = value["priority"]
+					p = int(value["priority"])
 					nearest=key
+						#print "try: "+key+" "+str(n)+" "+str(p)
 				except:
 					print exception
+					
+	if n == 999999:
+		return None,None,None,None,None,None,None,None,None,None,None
 	
 	return nearest,n,list[nearest]["lat"],list[nearest]["lon"],list[nearest]["active"],list[nearest]["body"],list[nearest]["text"],list[nearest]["priority"],list[nearest]["x"],list[nearest]["y"],list[nearest]["z"]
 		
-		
+def setStatus(nearest,distance,xbody,text,lat,long):
+	if this.nearest == None:
+		this.status['text'] = "Restart Tour" 
+		this.clipboard.grid_remove()
+		this.tick.grid_remove()
+		this.cross.grid_remove()
+		this.description.grid_remove()
+	else:
+		print this.nearest
+		print distance
+		this.status['text'] = this.nearest + " (" + str(distance) +"ly)"
+		this.status['url'] = 'https://www.edsm.net/show-system?systemName=%s' % quote_plus(nearest)
+		this.body['text']=xbody
+		this.description["text"] = text
+		this.label.grid()
+		this.status.grid()
+		this.clipboard.grid()
+		this.tick.grid()
+		this.cross.grid()
+		this.body.grid()
+		this.body_label.grid()
+		this.description.grid()
 	
 # Detect journal events
 def journal_entry(cmdr, system, station, entry):
 
 	if entry['event'] == 'FSDJump':
+		print 'FSDJump'
 		this.jumpsystem = { "x": entry["StarPos"][0], "y": entry["StarPos"][1], "z": entry["StarPos"][2], "name": entry["StarSystem"] }	
 		print this.jumpsystem
 		this.nearest,distance,lat,lon,active,body,text,priority,x,y,z = findNearest(this.jumpsystem,sites)
-		print nearest
-		print distance
-		this.status['text'] = nearest + " (" + str(distance) +"ly)"
-		this.status['url'] = 'https://www.edsm.net/show-system?systemName=%s' % quote_plus(nearest)
-		this.description["text"] = text
-		print "Commander Data"
+		setStatus(nearest,distance,body,text,lat,long)
+		
+		
 		
 	if entry['event'] == 'Location':
 		print "Location"
 		print entry
 		this.lastsystem = { "x": entry["StarPos"][0], "y": entry["StarPos"][1], "z": entry["StarPos"][2], "name": entry["StarSystem"] }
 		this.nearest,distance,lat,lon,active,body,text,priority,x,y,z = findNearest(this.lastsystem,sites)
-		print nearest
-		print distance
-		this.status['text'] = nearest + " (" + str(distance) +"ly)"
-		this.status['url'] = 'https://www.edsm.net/show-system?systemName=%s' % quote_plus(nearest)
-		this.description["text"] = text
-		print "Commander Data"
+		setStatus(nearest,distance,body,text,lat,long)
+
+		
 	
 	
 def edsmGetSystem(system):
@@ -194,11 +230,7 @@ def cmdr_data(data):
 	#print data
 	this.lastsystem = edsmGetSystem(data['lastSystem']['name'])
 	this.nearest,distance,lat,lon,active,body,text,priority,x,y,z = findNearest(this.lastsystem,sites)
-	print this.nearest
-	print distance
-	print "setting status"
-	this.status['text'] = this.nearest + " (" + str(distance) +"ly)"
-	this.status['url'] = 'https://www.edsm.net/show-system?systemName=%s' % quote_plus(nearest)
-	this.description["text"] = text
+	setStatus(nearest,distance,body,text,lat,long)
+
 	print "Commander Data"
 
